f9abce4deaf12691da5ab0996be95b074de6fa04
Salixe1214 <alexis.st-amand.1@ulaval.ca>
2021-01-04 10:14:05 -0500

    Ajout du mouvement du joueur

---
+++ projetAlpha/Assets/scripts/Player/mouvements.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * mouvements.cs
+ * Code pour les deplacements du joueur
+ * 4 decembre 2021
+ * Salixe
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// <c>mouvements</c> definie et gere les mouvements du joueur.
+/// </summary>
+public class mouvements : MonoBehaviour
+{
+    public float speed = 7f;
+
+    float angle;
+    Vector2 velocity;
+
+    new Rigidbody2D rigidbody;
+
+    /// <summary>
+    /// <c>Start</c> est appele a la premiere
+    /// update de frame.
+    /// </summary>
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    /// <summary>
+    /// <c>Update</c> est execute a chaque frame.
+    /// </summary>
+    void Update()
+    {
+        // Direction du joueur definie par les
+        // entre verticales et horizontales.
+        Vector2 inputDirection = new Vector2(Input.GetAxisRaw("Horizontal"), 
+                                   Input.GetAxisRaw("Vertical")).normalized;
+
+        // On ne change l'orientation que si le
+        // joueur entre activement une 
+        // orientation.
+        if(inputDirection.magnitude != 0)
+        {
+            angle = Mathf.Atan2(inputDirection.x, inputDirection.y) * Mathf.Rad2Deg;
+            transform.eulerAngles = Vector3.back * angle;
+        }
+
+        // Vitesse de deplacement
+        velocity = transform.up * speed * inputDirection.magnitude;
+    }
+
+    /// <summary>
+    /// <c>FixedUpdate</c> est execute a des
+    /// temps fixes.
+    /// </summary>
+    private void FixedUpdate()
+    {
+        // Deplacement du personnage devant lui.
+        rigidbody.MovePosition(rigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+}
