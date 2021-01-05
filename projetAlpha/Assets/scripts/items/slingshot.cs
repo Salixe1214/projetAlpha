5cef394bc30e97547802943e2e05b6fd5f4a5819
Salixe1214 <alexis.st-amand.1@ulaval.ca>
2021-01-05 12:23:21 -0500

    Ajout du sling shot

---
+++ projetAlpha/Assets/scripts/items/slingshot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slingshot : MonoBehaviour
+{
+    public GameObject m_pierre;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onUse(Vector2 position, Quaternion direction)
+    {
+        Instantiate(m_pierre, position, Quaternion.identity).transform.rotation = direction;
+    }
+}
