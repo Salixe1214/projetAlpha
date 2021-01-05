7a2d6104ca625d51c378ac1f1f9b5ae5e1888c97
MelpyX <maxime.gaudet.1@ulaval.ca>
2021-01-05 11:27:42 -0500

    Inplémentation du système de point de vie et de perte de point de vie. (qui est fixe et ne dépend pas de l'attaque de l'ennemie encore)

---
+++ projetAlpha/Assets/scripts/Player/Vie.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vie : MonoBehaviour
+{
+    public float secondesInvincibiliteApresCoup = 2;
+    public int pointDeVie = 10;
+    bool estInvincible = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pointDeVie <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D CollisionEnnemi)
+    {
+        if (!estInvincible)
+        {
+            pointDeVie -= 4;
+            StartCoroutine(invincibiliteFrame());
+        }
+    }
+
+    IEnumerator invincibiliteFrame()
+    {
+        if (!estInvincible)
+        {
+            estInvincible = true;
+            Debug.Log("est invincible");
+            yield return new WaitForSeconds(secondesInvincibiliteApresCoup);
+        }
+        estInvincible = false;
+        Debug.Log("est pas invincible");
+        yield return null;
+    }
+}
