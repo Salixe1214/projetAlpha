c9118e6b9669410fc824bd2a8dd6739ed63f4cab
Salixe1214 <alexis.st-amand.1@ulaval.ca>
2021-01-05 09:22:58 -0500

    Prefabs de test et ajout camera a la scene afin de pouvoir debug

---
+++ projetAlpha/Assets/testDebug/tests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tests : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            Debug.Log("mouse1");
+        }
+        if (Input.GetAxisRaw("Item1") != 0)
+        {
+            Debug.Log("Item 1");
+        }
+        if (Input.GetAxisRaw("Item2") != 0)
+        {
+            Debug.Log("Item 2");
+        }
+    }
+}
