e549227c74628cac8ef3d94d44a5f7e7955bee8d
MelpyX <maxime.gaudet.1@ulaval.ca>
2021-01-06 12:14:57 -0500

    Implémentation de l'UI et du bouton pause.

---
+++ projetAlpha/Assets/scripts/UI/PauseScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseScript : MonoBehaviour
+{
+    public Button Reprendre, Recommencer, Quitter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Reprendre.onClick.AddListener(ReprendreLaPartie);
+        Recommencer.onClick.AddListener(RecommencerLaPartie);
+        Quitter.onClick.AddListener(QuitterLaPartie);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Pause"))
+        {
+            ReprendreLaPartie();
+        }
+    }
+
+    void ReprendreLaPartie()
+    {
+        gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    void QuitterLaPartie()
+    {
+        gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    void RecommencerLaPartie()
+    {
+
+    }
+}
---
+++ projetAlpha/Assets/scripts/UI/canvasScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class canvasScript : MonoBehaviour
+{
+    public GameObject pause;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Mettre le menu pause inactif en commencant le jeu.
+        pause.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            pause.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+}
